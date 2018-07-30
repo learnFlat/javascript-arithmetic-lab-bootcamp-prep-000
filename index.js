123c1fa6d4569e225dc67c55cf79121f627c50ae
Cynthia Zheng <xzcynthiazheng@gmail.com>
2018-07-30 21:32:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,17 +15,17 @@ function divide(a,b) {
 }
 
 function inc(n) {
-  n++
+  return n++
 }
 
 function dec(n) {
-  n--
+  return n--
 }
 
 function makeInt(n) {
-  parseInt(n, 10)
+  return parseInt(n, 10)
 }
 
 function preserveDecimal(n) {
-  parseFloat(n)
+  return parseFloat(n)
 }
